03080afea4f771e4feae428fb19cd9737d88c1b6
Anson <zmz211@gmail.com>
2022-09-05 18:03:41 -0700

    mongoose

---
+++ dist/server.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var express = require('express');
+
+var mongoose = require('mongoose');
+
+var articleRouter = require('./routes/articles');
+
+var app = express();
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+app.set('view engine', 'ejs');
+app.use('/articles', articleRouter);
+app.get('/', function (req, res) {
+  var articles = [{
+    title: 'Test Article',
+    createdAt: new Date(),
+    description: 'Test description'
+  }, {
+    title: 'Test Article 2',
+    createdAt: new Date(),
+    description: 'Test description 2'
+  }];
+  res.render('articles/index', {
+    articles: articles
+  });
+});
+app.listen(5600);
